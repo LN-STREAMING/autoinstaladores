cddb11713ed5f4f2635ca30ff53d01358b15be92
LN-STREAMING <158014427+LN-STREAMING@users.noreply.github.com>
2025-02-24 00:42:27 -0300

    Update ikabot.sh

--- ikabot.sh
+++ ikabot.sh
@@ -1,42 +1,36 @@
 #!/bin/bash
 
-# Função para exibir mensagens formatadas
-log() {
-    echo -e "\e[1;32m[INFO] $1\e[0m"
-}
+# Configuração para evitar prompts interativos
+export DEBIAN_FRONTEND=noninteractive
+export NEEDRESTART_MODE=a
 
-# Atualizar pacotes do sistema
-log "Atualizando pacotes do sistema..."
-sudo apt update -y && sudo apt upgrade -y
+# Atualizar pacotes
+echo "Atualizando pacotes..."
+sudo apt update && sudo apt upgrade -y
 
-# Instalar dependências do Python e GCC
-log "Instalando dependências..."
-sudo apt install -y python3 python3-dev python3-pip python3-venv gcc
+# Instalar pacotes necessários
+echo "Instalando dependências..."
+sudo apt install -y python3 python3-dev python3-pip python3-venv gcc git wget
 
-# Baixar e instalar get-pip.py
-log "Baixando e instalando get-pip.py..."
-sudo wget -q https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py
+# Baixar e instalar o get-pip.py
+echo "Baixando e instalando o gerenciador de pacotes pip..."
+wget -q https://bootstrap.pypa.io/get-pip.py
+sudo python3 get-pip.py
 sudo pip3 install psutil
 
-# Clonar o repositório Ikabot e instalar
-log "Baixando e instalando Ikabot..."
+# Clonar o repositório do Ikabot e instalar
+echo "Baixando e instalando o Ikabot..."
 sudo git clone https://github.com/Ikabot-Collective/ikabot
 cd ikabot
 sudo python3 -m pip install --user -e .
 
-# Solicitar email e senha
-echo -e "\e[1;34mDigite seu email para o Ikabot:\e[0m"
-read EMAIL
-echo -e "\e[1;34mDigite sua senha para o Ikabot:\e[0m"
-read -s SENHA  # O "-s" oculta a senha enquanto o usuário digita
+# Solicitar email e senha do usuário
+read -p "Digite seu e-mail do Ikabot: " email
+read -s -p "Digite sua senha do Ikabot: " senha
+echo ""
 
-# Executar Ikabot automaticamente com os dados fornecidos
-log "Executando Ikabot..."
-python3 -m ikabot "$EMAIL" "$SENHA"
+# Executar o Ikabot com as credenciais fornecidas
+echo "Iniciando o Ikabot..."
+python3 -m ikabot "$email" "$senha"
 
-# Exibir mensagem final
-log "Instalação concluída com sucesso!"
-echo -e "\e[1;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
-echo -e "\e[1;32mIkabot foi instalado e está rodando com suas credenciais!\e[0m"
-echo -e "\e[1;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━\e[0m"
+echo "Instalação concluída! O Ikabot está rodando."
