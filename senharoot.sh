01e75f8bdeabb4674035785306d6c4ee9b808b0b
LN-STREAMING <158014427+LN-STREAMING@users.noreply.github.com>
2025-04-27 16:56:17 -0300

    Update senharoot.sh

--- senharoot.sh
+++ senharoot.sh
@@ -5,6 +5,7 @@
 echo "Digite a nova senha para o usuário root:"
 read -s nova_senha
 
+# Usando sudo para garantir que o comando tenha permissão de root
 echo "root:$nova_senha" | sudo chpasswd
 
 echo "Senha do root alterada com sucesso!"
